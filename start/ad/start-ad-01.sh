ee551c0cc393e9519acc77b5f345f6826f4fa2e0
Andrei Savenko <super@startspace.ru>
2015-03-26 15:34:37 +0300

    Change txt file name

--- start/ad/start-ad-01.sh
+++ start/ad/start-ad-01.sh
@@ -3,20 +3,19 @@
 # export AWS_ACCESS_KEY="Your-Access-Key"
 # export AWS_SECRET_KEY="Your-Secret-Key"
 
-today=`date +"%d-%m-%Y","%T"`
+today=`date +"%m-%d-%Y"+"%T"`
 logfile="/var/log/start-instances.log"
 
 # Grab all Instance IDs for START action and export the IDs to a text file
-aws ec2 describe-instances --filters Name=tag-key,Values=bash-start Name=tag-value,Values=01-00 Name=tag-value,Values=ad --query Reservations[*].Instances[$
+aws ec2 describe-instances --filters Name=tag-key,Values=bash-start Name=tag-value,Values=01-00 Name=tag-value,Values=ad --query Reservations[*].Instances[*].[InstanceId] --output text > /tmp/start_ad_instance_info.txt
 
-# Take list of starting instances
-for instance_id in $(cat /tmp/instance_info.txt)
-
+# Take a snapshot of all volumes attached to this instance
+for instance_id in $(cat /tmp/start_ad_instance_info.txt)
 do
+description="start-$(date +%Y-%m-%d)"
 
 # Start instances
 startresult=$(aws ec2 start-instances --instance-ids $instance_id)
 
-# Put info into log file
-echo Started $today instances by AWS CLI with command $startresult >> $logfile
+echo Started  $today instances $startresult >> $logfile
 done
